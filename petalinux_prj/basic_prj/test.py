70b19d08d8d503ab4ec92fceb403ba7ab09fcf2a
fabioc9675 <fabian.castano@udea.edu.co>
2024-01-11 02:20:04 -0500

    modification to use a python function with spi

--- petalinux_prj/basic_prj/test.py
+++ petalinux_prj/basic_prj/test.py
@@ -4,6 +4,33 @@ import ctypes
 # Cargar la biblioteca compartida
 my_library = ctypes.CDLL('./libmylibrary.so')
 
+# Declarar el tipo de los argumentos y del resultado de la función HAL_Spi_ReadWrite
+#my_library.HAL_Spi_ReadWrite.argtypes = [ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+my_library.HAL_Spi_ReadWrite.restype = ctypes.c_int
+
+tx_buf = (ctypes.c_char * 4)('A'.encode('utf-8'),2,3,4)
+rx_buf = (ctypes.c_char * 4)()
+
+status = my_library.HAL_Spi_ReadWrite(ctypes.byref(tx_buf), ctypes.byref(rx_buf), 4)
+
+if status == 0:
+    print("Operación realizada correctamente.")
+    print("Datos recibidos:", list(rx_buf))
+else:
+    print("Error:", status)
+
+tx_buf = (ctypes.c_char * 4)('F'.encode('utf-8'),9,8,7)
+rx_buf = (ctypes.c_char * 4)()
+
+status = my_library.HAL_Spi_ReadWrite(ctypes.byref(tx_buf), ctypes.byref(rx_buf), 4)
+
+if status == 0:
+    print("Operación realizada correctamente.")
+    print("Datos recibidos:", list(rx_buf))
+else:
+    print("Error:", status)
+
+
 # Declarar el tipo de los argumentos y del resultado de la función sum
 my_library.sum.argtypes = [ctypes.c_int, ctypes.c_int]
 my_library.sum.restype = ctypes.c_int
